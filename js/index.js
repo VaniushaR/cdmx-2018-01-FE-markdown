7ef7f2031f65129b6bfd9a60ba9a13dddef2acfb
VaniushaR <vaniusha.ra@gmail.com>
2018-09-10 14:59:37 -0500

    Fetch

--- js/index.js
+++ js/index.js
@@ -1,91 +1,103 @@
-console.log('hello word');
-let userFile = 'js/read.md';
+console.log('active');
+//files for test functions
+const userFile = 'js/read.md';
+const user2File = 'js/read.txt';
+let htmlFile = {};
+let markedFile;
+let totalLinksFounded = 0;
+//modules requirements Core Node Modules
 const fs = require('fs');
 const pathExt = require('path');
-const notValid = 'Not valid type file';
-const mdLinks = (path, callback) => {
-  console.log('file type' + pathExt.extname(path));
-  const userTypeFile = pathExt.extname(path);
+//modules requirements form another NPM libraries
+const myMarked = require('marked');
+myMarked.setOptions({});
 
+//Function to validate links of .md files
+const mdLinks = (path, options) => {
+  //console.log('file type' + pathExt.extname(path)); Revisando el tipo de extensión
+  const userTypeFile = pathExt.extname(path);
   if (userTypeFile !== '.md') {
-    console.log('Incorrect type file!' + userTypeFile);
-    return notValid;
+    //return to end the process
+    return console.log(
+      'Incorrect type file! please give a .md file to review' +
+        userTypeFile +
+        ' is not an accepted file type'
+    );
   } else {
+    //notice that the type of file es .md
     console.log('correct type file' + userTypeFile);
     fs.readFile(path, 'utf8', (error, file) => {
+      //UTF-8 (8-bit Unicode Transformation Format) codification of longitude variable. Others could be utf16 or unicode.
       if (error) {
         console.log('Hay un error' + error);
+        //Error Handling refers to how Express catches and processes errors that occur both synchronously and asynchronously, it's a parameter by defaulf of readFile
       } else {
-        console.log(file);
-        callback();
+        linkSearcher(file);
       }
     });
   }
 };
 
-mdLinks(userFile, function() {
-  console.log('hola vania');
-});
+mdLinks(
+  userFile,
+  (options = (stats, validate) => {
+    console.log('Leyendo función de callback');
+  })
+);
+
+//Function to search tag a and href contents on a file, will be invoked throw callback
+const linkSearcher = file => {
+  //Variables to keep matches
+  htmlFile = myMarked(file);
+  //console.log(htmlFile);
+  //Give a regular expression to find the links in the href html tag, include lower and upper case
+  const regExprLink = /href="(.*?)"/gi;
+  //Give a regular expression to find the <a> html tag content, include lower and upper case
+  const regExprATag = />[^<]*<[ ]*\/a[ ]*>/gi;
+  //create new variables to keep the matches finded with the patterns of the regular expressions:
+  const linkMatchesKeeper = htmlFile.match(regExprLink);
+  //console.log('Links', linkMatchesKeeper);
+  const aTagMatchesKeeper = htmlFile.match(regExprATag);
+  //console.log('A tag:', aTagMatchesKeeper);
+  stringCleaner(linkMatchesKeeper);
+  stringTagCleanner(aTagMatchesKeeper);
+  console.log('Characters Extension: ', htmlFile.length);
+  console.log(typeof htmlFile);
+};
+//cleaner*
+const stringCleaner = stringstoClean => {
+  const arrayOfLinkStrings = stringstoClean.map(item => item.slice(6, -1));
+  console.log(arrayOfLinkStrings);
+  console.log(arrayOfLinkStrings.length);
+  //linkValidator(arrayOfLinkStrings);
+};
+
+const stringTagCleanner = stringstoClean => {
+  const arrayOfTagStrings = stringstoClean.map(item => item.slice(1, -4));
+  console.log(arrayOfTagStrings);
+  console.log(arrayOfTagStrings.length);
+  //addLinkDescription(arrayOfTagStrings, arrayOfTagStrings);
+};
+const linkValidator = (arrayOfLinks) => {
+console.log(arrayOfLinks);
+arrayOfLinks.forEach(item => {
+  fetch(item).then(response => response.stats()
+  .then(status => console.log(status))
+);
+
+}
+
+
+
+
+// mdLinks(user2File, function() {
+//   console.log('Leyendo el Callback');
+// });
+
+//export to testing
+module.exports = {
+  mdLinks
+};
 
 // var data = fs.readFile('js/read.txt', 'utf8');
 // console.log(data);
-
-//Segundo intento
-// var express = require('express');
-// var app = express();
-
-// app.get = ('/', (req, res) => {
-//   res.send('Hello World')
-// })
-
-// app.listen(3000)
-
-// const searchingLinksFile = inputFile => {
-//   (fs = require('fs')),
-//     (readline = require('readline')),
-//     (instream = fs.createReadStream(imputFile)),
-//     (outstream = new (require('stream'))()),
-//     (rl = readline.createInterface(instream, outstream));
-
-//   rl.on('line', function(line) {
-//     console.log(line);
-//   });
-//   rl.on('close', function(line) {
-//     console.log(line);
-//     console.log('donde reading file.');
-//   });
-// };
-// searchingLinksFile('read.txt');
-
-//Primer intento:
-// const mdLinks = require('md-links');
-
-// mdLinks('./some/example.md')
-//   .then(links => {
-//     // => [{ href, text, file }]
-//   })
-//   .catch(console.error);
-
-// mdLinks('./some/example.md', { validate: true })
-//   .then(links => {
-//     // => [{ href, text, file, status, ok }]
-//   })
-//   .catch(console.error);
-
-// mdLinks('./some/example.md', { stats: true })
-//   .then(links => {
-//     // => [{total, unique }]
-//   })
-//   .catch(console.error);
-
-// mdLinks('./some/example.md', { stats: true, validate: true })
-//   .then(links => {
-//     // => [{total, unique, broken }]
-//   })
-//   .catch(console.error);
-
-// mdLinks('./some/dir')
-//   .then(links => {
-//     // => [{ href, text, file }]
-//   })
-//   .catch(console.error);
